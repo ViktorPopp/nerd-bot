fb1a57e4970bf69e0a3b3278f2b89e527feafbff
Viktor Popp <viktorpopp.dev@gmail.com>
2025-02-19 16:31:15 +0100

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+const { Client, IntentsBitField } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on('ready', (c) => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
